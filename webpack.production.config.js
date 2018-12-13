f4b50313ce9642dfc97c293dd0f0410a19eb963e
Sean Jacobsma <skj4@gold09.cs.calvin.edu>
2018-12-13 15:25:06 -0500

    updated production server

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -14,7 +14,7 @@ module.exports = {
     module: {
         loaders: [
             { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader" },
-            { test: /\.css$/,  loader: 'style!css?modules!postcss' }
+            { test: /\.css$/,  loader: 'style-loader!css-loader' }
         ]
     },
     postcss: [
